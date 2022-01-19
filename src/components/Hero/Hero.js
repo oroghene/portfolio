91188947e1eb8c4c4e93ac55f4c4aa3b03de4b20
Oroghene Emudainohwo <oemudainohwo@gmail.com>
2022-01-19 04:19:59 -0500

    remove button from Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,6 @@ const Hero = (props) => (
         <SectionText>
         Welcome! My name is Oroghene Emudainohwo. I am currently studying Electrical Engineering in Drexel University with a minor in computer science. I have a greater passion for computer science, but electrical engineering is also an exciting field. I am a student of the Pennoni Honors College where I have explored fascinating thought-provoking topics such as fashion and history.
         </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
     </Section>
   </>
